d8079ca72301851cda2eef1c3685f04e62a595a6
Mahyar Abbaspour <m.abbaspour@pishro.computer>
2016-07-13 19:16:52 +0430

    Add YAML mode

--- init.el
+++ init.el
@@ -125,7 +125,7 @@
 	("c3c0a3702e1d6c0373a0f6a557788dfd49ec9e66e753fb24493579859c8e95ab" default)))
  '(package-selected-packages
    (quote
-	(fic-mode flycheck markdown-mode evil-commentary evil go-mode))))
+	(yaml-mode fic-mode flycheck markdown-mode evil-commentary evil go-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
