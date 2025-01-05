9366db8a7a68ca7e032facdd7271d7f7e7d17a5e
Mahyar Abbaspour <mahyar.abaspour@gmail.com>
2025-01-05 02:03:06 +0100

    Add neovim config

---
+++ nvim/.config/nvim/lua/config/formatting.lua
@@ -0,0 +1,26 @@
+-- Indentation settings for using 4 spaces instead of tabs.
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.api.nvim_create_augroup("indentation", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+    group = "indentation",
+    pattern = { "lua", "ruby", "html", "vue", "yaml", "javascript" },
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+    end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    group = "indentation",
+    pattern = { "c", "cpp", "go" },
+    callback = function()
+        vim.opt_local.tabstop = 4
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.expandtab = false
+    end,
+})
+
+vim.opt.cinoptions = ":0"
---
+++ nvim/.config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,11 @@
+require('lint').linters_by_ft = {
+  markdown = {'vale'},
+}
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  callback = function()
+    -- try_lint without arguments runs the linters defined in `linters_by_ft`
+    -- for the current filetype
+    require("lint").try_lint()
+  end,
+})
