2947d0b11438d48a5333980f573d11617addb41b
Mahyar Abbaspour <mahyar.abaspour@gmail.com>
2026-03-29 22:09:34 +0200

    Remove some useless mappings

--- nvim/.config/nvim/lua/config/mappings.lua
+++ nvim/.config/nvim/lua/config/mappings.lua
@@ -19,10 +19,6 @@ vim.api.nvim_set_keymap("n", "<S-down>", "<C-W><down>", {noremap = true, silent 
 vim.api.nvim_set_keymap("n", "<S-left>", "<C-W><left>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<S-right>", "<C-W><right>", {noremap = true, silent = true})
 
--- Short cut to edit and source vimrc
-vim.api.nvim_set_keymap("n", "<leader>ev", ":edit $MYVIMRC<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>sv", ":source $MYVIMRC<CR>", {noremap = true, silent = false})
-
 -- Abbreviations to prevent typos in command mode
 vim.cmd("cnoreabbrev W w")
 vim.cmd("cnoreabbrev Wq wq")
